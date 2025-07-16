0d76d0853fe7a38cf3d134777ebb21dd7151e5bd
Dennes Torres <Cadm_DennesDeOliveira@cofcointernational.com>
2025-07-16 15:48:47 +0200

    removing test

--- test.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-echo  starting
-
-. "$PSScriptRoot\reusable.ps1"
-
-Convert-PBIRByPathToByConnection -PBIRFilePath "$PSScriptRoot\Lakehouse Maintenance Report.Report\definition.pbir" -WorkspaceName "OnyxTools" -SemanticModelId "7fcc1088-feb6-4cee-9ec0-72cb893113bb" -SemanticModelName "Lakehouse Maintenance Report"
